07944abe0772a04eda171150c845b3bbf8980bd4
kalai02484 <kalai02484@gmail.com>
2025-12-11 21:52:01 +0530

    Header and Footer Added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      Footer Component
+    <div className='bg-indigo-950 text-white p-4 text-center'>
+        &copy; 2025 MyStore. All rights reserved.      
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar Component
+    <div className='bg-blue-950 text-white p-4 shadow-md flex justify-between items-center'>
+      <div className='text-2xl font-bold'>MyStore</div>
+      <div className='space-x-4'>
+        <Link to='/' className='hover:underline'>Home</Link>
+        <Link to='/cart' className='hover:underline'>Cart</Link>
+      </div>
     </div>
   )
 }

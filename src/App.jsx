bc84943d396c678f50b25d7af22a76c6cc20fafc
kalai02484 <kalai02484@gmail.com>
2025-12-11 21:43:59 +0530

    Router Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ProductList from './pages/ProductList'
+import Cart from './pages/Cart'
+import PageNotFound from './pages/PageNotFound'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div>
-      App Component
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<ProductList />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   )
 }
 
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+      Product Card Component
+    </div>
+  )
+}
+
+export default ProductCard
